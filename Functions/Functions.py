e07a266edeb7d7cf7f1645bdd9af6d57374df582
amara-kernel <amara.kernel@gmail.com>
2022-12-21 07:52:36 +0800

    Added/Updated

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -31,3 +31,42 @@ sqroot = math.sqrt(49)
 print(sqroot)
 print(math.pow(2, 4))
 
+
+# Importing a module
+import example
+
+print(example.add(4, 66))
+
+
+# Rename a module
+import example as ex
+
+print(ex.add(10,22))
+
+
+# Import specific function or name
+from example import add
+
+print(add(100,200))
+
+# Import names
+from example import *
+print(add(250, 250))
+#__myprint("Amara") - * will not import functions beginning with underscore (private functions).
+
+from example import __myprint
+__myprint("Sreenivas")
+
+"""Here, we have imported all the definitions from the math module. This includes all names visible in our 
+scope except those beginning with an underscore(private definitions).
+
+Importing everything with the asterisk (*) symbol is not a good programming practice. 
+This can lead to duplicate definitions for an identifier. It also hampers the readability of our code.
+"""
+
+# In Python, we can use the dir() built in function to list all the function names in a module.
+# Default Python attributes associated with the module are with __<name>__
+print("All function names in example module: ", dir(example))
+print("Name of the module : ", ex.__name__)
+
+
---
+++ Functions/example.py
@@ -0,0 +1,12 @@
+# example is a module.
+
+word = "India"
+
+
+def add(x, y):
+    add = x + y
+    return add
+
+
+def __myprint(name):
+    print("Printing from example : ", name)
